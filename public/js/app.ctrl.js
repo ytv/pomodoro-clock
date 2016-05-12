70d7eda224143abd7e98afd5765f94a61cb05846
ytv <sillying@gmail.com>
2016-05-12 12:04:08 -0700

    removed the need to index/reindex

--- public/js/app.ctrl.js
+++ public/js/app.ctrl.js
@@ -1,20 +1,14 @@
 app.controller('AppCtrl', function($scope) {
     $scope.count = 0;
-    $scope.countdownTimers = [{index: 0}];
+    $scope.countdownTimers = [{}];
 
     $scope.addTimer = function() {
         $scope.count++;
-        $scope.countdownTimers.push({index: $scope.count});
+        $scope.countdownTimers.push({});
     };
 
-    $scope.reIndex = function() {
-        for (var i = 0; i < $scope.countdownTimers.length; i++) {
-            $scope.countdownTimers[i].index = i;
-        }
-    };
-
-    $scope.removeTimer = function(index) {
-        $scope.countdownTimers.splice(index, 1);
+    $scope.removeTimer = function(countdownTimer) {
+        $scope.countdownTimers.splice($scope.countdownTimers.indexOf(countdownTimer), 1);
         $scope.reIndex();
     }
 
